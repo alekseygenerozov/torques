9619a051bb2257a3d6c179d0ac1dec220fde228d
alekseygenerozov <aleksey.generozov@gmail.com>
2019-01-08 17:54:11 -0700

    Paths

--- run_disk_torque.py
+++ run_disk_torque.py
@@ -10,13 +10,13 @@ idx=sys.argv[4]
 dd=1.0
 i=0
 
-No=100
+No=1000
 while (dd>0.05) and (i<6):
-	bc.bash_command('/home/aleksey/code/c/torques/rebound_disk {0} {1} {2} {3} {4} {5}'.format(e1, a1, ang, idx, 0,  No))
-	bc.bash_command('/home/aleksey/code/c/torques/rebound_disk {0} {1} {2} {3} {4} {5}'.format(e1, a1, ang, idx, 1,  No))
+	# bc.bash_command('/home/aleksey/code/c/torques/rebound_disk {0} {1} {2} {3} {4} {5}'.format(e1, a1, ang, idx, 0,  No))
+	# bc.bash_command('/home/aleksey/code/c/torques/rebound_disk {0} {1} {2} {3} {4} {5}'.format(e1, a1, ang, idx, 1,  No))
 
-	# bc.bash_command('/projects/alge9397/code/c/torques/rebound_disk {0} {1} {2} {3} {4} {5}'.format(e1, a1, ang, idx, 0,  No))
-	# bc.bash_command('/projects/alge9397/code/c/torques/rebound_disk {0} {1} {2} {3} {4} {5}'.format(e1, a1, ang, idx, 1,  No))
+	bc.bash_command('/projects/alge9397/code/c/torques/rebound_disk {0} {1} {2} {3} {4} {5}'.format(e1, a1, ang, idx, 0,  No))
+	bc.bash_command('/projects/alge9397/code/c/torques/rebound_disk {0} {1} {2} {3} {4} {5}'.format(e1, a1, ang, idx, 1,  No))
 
 	tdot1=np.genfromtxt('tau_N1000_a_{0}_{1}_{2}_{3}'.format(e1, a1, ang, idx))
 	tdot2=np.genfromtxt('tau_N1000_b_{0}_{1}_{2}_{3}'.format(e1, a1, ang, idx))
