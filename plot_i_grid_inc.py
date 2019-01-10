e9c00f22d435324b999869c04bb07470b2758317
alekseygenerozov <aleksey.generozov@gmail.com>
2019-01-10 11:23:25 -0700

    Run inclination grid.

--- plot_i_grid_inc.py
+++ plot_i_grid_inc.py
@@ -10,45 +10,46 @@ import re
 
 import sys
 
-ang_test=np.array([0.2, 0.5, 1.0, 2.0, 4.0, 8.0, 16.0])
+ang_test=np.array([0.2, 0.4, 1.0, 2.0, 4.0, 8.0, 16.0])
 
 m=2.5e-7
 e=0.7
 Ntrials=5
 tag=sys.argv[1]
-loc='//home/aleksey/Dropbox/projects/disk_binaries/torque_data_2/grid_inc/'
+loc='//home/aleksey/Dropbox/projects/disk_torque/torque_data_2/grid_disk_inc/'
 
 
-ti=np.zeros([len(ang_test), Ntrials])
-ti_avg=np.zeros(len(ang_test))
-ti_err=np.zeros(len(ang_test))
+deriv=np.zeros([len(ang_test), Ntrials])
+derivb=np.zeros([len(ang_test), Ntrials])
+num=np.zeros(len(ang_test))
 
 # tib=np.zeros([len(a_test), len(ang_test)])
 
-a1=1
+a1=1.0
 for jj,ang in enumerate(ang_test):
 	for idx in range(1,Ntrials+1):
-		tmp=np.genfromtxt(loc+'{2}_N1000_a_0.7_{0}_{1}_{3}'.format(a1, ang, tag, idx))
-		jo=m*(a1*(1.-e**2))**0.5
+		print loc+'{2}_N1000_a_0.7_{0}_{1:.1}_{3}'.format(a1, float(ang), tag, idx),jj
+		deriv[jj,idx-1]=np.genfromtxt(loc+'{2}_N1000_a_0.7_{0}_{1}_{3}'.format(a1, ang, tag, idx))
+		derivb[jj,idx-1]=np.genfromtxt(loc+'{2}_N1000_b_0.7_{0}_{1}_{3}'.format(a1, ang, tag, idx))
 
-		if tag=='i':
-			tmp=tmp/m
-			ti[jj,idx-1]=(ang*np.pi/180/tmp)
-		else:
-			ti[jj,idx-1]=jo/tmp
-
-ti_avg=np.mean(ti, axis=1)
-# ti_err=np.std(ti, axis=2)
-
-
+		num[jj]=m*(a1*(1.-e**2))**0.5
 
 fig,ax=plt.subplots(figsize=(15,8))
 ax.set_xlim(0., 20.)
 ax.set_xlabel(r'$i$')
 ax.set_ylabel(r'$t$')
+
+ti_avg=num/np.mean(deriv, axis=1)
+# ti_err=np.std(ti, axis=2)
 ax.semilogy(ang_test, abs(ti_avg))
-ax.semilogy(ang_test[ti_avg<0], abs(ti_avg[ti_avg<0]), 's')
-ax.semilogy(ang_test[ti_avg>0], abs(ti_avg[ti_avg>0]), 'D')
+ax.semilogy(ang_test[ti_avg<0], abs(ti_avg[ti_avg<0]), 'ks')
+ax.semilogy(ang_test[ti_avg>0], abs(ti_avg[ti_avg>0]), 'kD')
+
+ti_avg=num/np.mean(derivb, axis=1)
+ax.semilogy(ang_test, abs(ti_avg))
+ax.semilogy(ang_test[ti_avg<0], abs(ti_avg[ti_avg<0]), 'rs')
+ax.semilogy(ang_test[ti_avg>0], abs(ti_avg[ti_avg>0]), 'rD')
+ax.annotate(r'$t_{sec,i}$=2.5\times 10^4')
 
 
 fig.savefig('{0}_grid_disk_inc.pdf'.format(tag))
---
+++ run_disk_torque_inc.py
@@ -0,0 +1,30 @@
+from bash_command import bash_command as bc
+import numpy as np
+import sys
+
+e1=sys.argv[1]
+a1=sys.argv[2]
+ang=sys.argv[3]
+idx=sys.argv[4]
+# offset=sys.argv[5]
+dd=1.0
+i=0
+
+No=1000
+while (dd>0.05) and (i<4):
+	# bc.bash_command('/home/aleksey/code/c/torques/rebound_disk {0} {1} {2} {3} {4} {5}'.format(e1, a1, ang, idx, 0,  No))
+	# bc.bash_command('/home/aleksey/code/c/torques/rebound_disk {0} {1} {2} {3} {4} {5}'.format(e1, a1, ang, idx, 1,  No))
+
+	bc.bash_command('/projects/alge9397/code/c/torques/rebound_disk_inc {0} {1} {2} {3} {4} {5}'.format(e1, a1, ang, idx, 0,  No))
+	bc.bash_command('/projects/alge9397/code/c/torques/rebound_disk_inc {0} {1} {2} {3} {4} {5}'.format(e1, a1, ang, idx, 1,  No))
+
+	tdot1=np.genfromtxt('tau_N1000_a_{0}_{1}_{2}_{3}'.format(e1, a1, ang, idx))
+	tdot2=np.genfromtxt('tau_N1000_b_{0}_{1}_{2}_{3}'.format(e1, a1, ang, idx))
+	dd=abs((tdot1-tdot2)/tdot1)
+	idot1=np.genfromtxt('i_N1000_a_{0}_{1}_{2}_{3}'.format(e1, a1, ang, idx))
+	idot2=np.genfromtxt('i_N1000_b_{0}_{1}_{2}_{3}'.format(e1, a1, ang, idx))
+	dd=max(dd, abs((idot1-idot2)/idot1))
+	No=No*2
+	i+=1
+	print e1, a1, ang, i-1,No/2, abs((tdot1-tdot2)/tdot1), abs((idot1-idot2)/idot1)
+	sys.stdout.flush()
