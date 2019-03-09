cc3a125ea766c7a417152d8ac5be4c4c61311f14
alekseygenerozov <aleksey.generozov@gmail.com>
2019-03-09 15:58:34 -0700

    Adding plotting routing to repo.

---
+++ plot_i_grid_2d.py
@@ -0,0 +1,77 @@
+import seaborn as sns
+from labelLine import labelLines
+import matplotlib.pyplot as plt
+import numpy as np
+
+import matplotlib.colors as colors
+from matplotlib import ticker
+from latex_exp import latex_exp
+import re
+
+import sys
+
+ang_test=np.arange(5., 91, 5)
+e_test=np.arange(0.1, 1.01,0.1)
+e_test[-1]=0.99
+
+
+m=2.5e-7
+Ntrials=5
+tag=sys.argv[1]
+a1=sys.argv[2]
+loc='//home/aleksey/Dropbox/projects/disk_torque/torque_data_2/dat_q-1.6/reg/'
+
+deriv=np.zeros([len(e_test), len(ang_test)])
+derivb=np.zeros([len(e_test), len(ang_test)])
+ti_avg=np.zeros([len(e_test), len(ang_test)])
+tib_avg=np.zeros([len(e_test), len(ang_test)])
+num=np.zeros([len(e_test), len(ang_test)])
+
+norm=1.0/m
+for ii,e1 in enumerate(e_test):
+	for jj,ang in enumerate(ang_test):
+		# for idx in range(1,Ntrials+1):
+		deriv_norm[ii,jj]=(j(a1, e1)/tsec)
+		lab=r'$\tau/(j/t_{sec})$'
+		if tag=='i':
+			# num[ii, jj]=ang*np.pi/180
+			deriv_norm[ii,jj]=(2.0*np.pi/tsec)
+			lab=r"$i_e'/(2 \pi/t_{sec})$"
+
+		deriv[ii,jj]=np.mean(prec_rate['{3}_a_e{0}_a{1}_o{2}_q-1.6_ein0.9_dt1.8'.format(e1, a1, ang, tag)])*norm
+		derivb[ii,jj]=np.mean(prec_rate['{3}_b_e{0}_a{1}_o{2}_q-1.6_ein0.9_dt1.8'.format(e1, a1, ang, tag)])*norm
+		# if np.isnan(deriv[ii, jj]):
+		# 	deriv[ii,jj]=derivb[ii, jj]
+
+deriv_avg=deriv
+
+
+
+fig,ax=plt.subplots(figsize=(15,8))
+ax.set_xlim(2.5,92.5)
+ax.set_ylim(0.05, 1.05)
+ax.set_xlabel(r'$\bar{\omega}$')
+ax.set_ylabel(r'$a$')
+
+for ii,e1 in enumerate(e_test):
+	for jj,ang in enumerate(ang_test):
+		tmp=abs(deriv_avg[ii,jj]/deriv_norm[ii,jj])
+		if tmp>=0.01:
+			plt.text(ang, e1, '{0:.2f}'.format(abs(deriv_avg[ii,jj]/deriv_norm[ii,jj])), fontsize=12, horizontalalignment='center')
+		else:
+			plt.text(ang, e1, '{0:.0e}'.format(abs(deriv_avg[ii,jj]/deriv_norm[ii,jj])), fontsize=12, horizontalalignment='center')
+
+
+
+tlist=[-1.0e6, -1.0e5, -1.0e4, -1.0e3,1.0e3, 1.0e4, 1.0e5, 1.0e6]
+tlist_tex=map(latex_exp.latex_exp, tlist)
+tlist_tex=[re.sub('1.0 \\\\times\s', '', tt) for tt in tlist_tex] 
+
+ang_ords=np.append(ang_test-2.5, ang_test[-1]+2.5)
+e_ords=np.append(e_test-0.05, e_test[-1]+0.05)
+p1=ax.pcolormesh(ang_ords, e_ords, np.abs(deriv_avg/deriv_norm), cmap='RdBu_r',norm=colors.SymLogNorm(linthresh=100, vmin=-1.0e6, vmax=1.0e6))
+
+# plt.colorbar(p1, label=r'$log(t_i)$')
+cbar=fig.colorbar(p1, ax=ax, ticks=tlist, label=r'$\tau$')
+cbar.ax.set_yticklabels(tlist_tex) 
+fig.savefig('{0}_a{1}_grid_disk_2d.pdf'.format(tag, a1))
