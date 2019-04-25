8e91b7a9ebb47b9040eef98513ccabaeef74b42f
alekseygenerozov <aleksey.generozov@gmail.com>
2019-04-25 15:46:57 -0600

    More paths

--- pipeline_2orbit.py
+++ pipeline_2orbit.py
@@ -1,5 +1,6 @@
 import subprocess
 import os
+import shlex
 
 def bash_command(cmd):
 	'''Run command from the bash shell'''
@@ -7,8 +8,11 @@ def bash_command(cmd):
 	return process.communicate()[0]
 
 pre=os.path.join(os.path.dirname(__file__))
-print pre
+f=open(pre+'/Makefile','r')
+rpath=((f.read().split('\n')[1]).split('='))[-1]
+
 bash_command('python {0}/elems_2orbit.py 0.05'.format(pre))
 bash_command('python {0}/elems_2orbit.py 0.1'.format(pre))
 bash_command('ln -s {0}/template_2orbit.sh'.format(pre))
 bash_command('python {0}/gen_grid_2orbit.py'.format(pre))
+bash_command('ln -s {0}/librebound.so'.format(rpath))
