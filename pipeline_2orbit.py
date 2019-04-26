4f58230ed10e1ceac32ce209fa1ff4f6c425e7a1
alekseygenerozov <aleksey.generozov@gmail.com>
2019-04-25 18:22:12 -0600

    Fixing pipeling

--- pipeline_2orbit.py
+++ pipeline_2orbit.py
@@ -13,8 +13,8 @@ f=open(pre+'/Makefile','r')
 rpath=((f.read().split('\n')[1]).split('='))[-1]
 a=float(sys.argv[1])
 
-bash_command('python {0}/elems_2orbit.py 0.05 {0}'.format(pre, a))
-bash_command('python {0}/elems_2orbit.py 0.1 {0}'.format(pre, a))
+bash_command('python {0}/elems_2orbit.py 0.05 {1}'.format(pre, a))
+bash_command('python {0}/elems_2orbit.py 0.1 {1}'.format(pre, a))
 bash_command('ln -s {0}/template_2orbit.sh'.format(pre))
-bash_command('python {0}/gen_grid_2orbit.py {0}'.format(pre, 0.99*a))
+bash_command('python {0}/gen_grid_2orbit.py {1}'.format(pre, 0.99*a))
 bash_command('ln -s {0}/librebound.so'.format(rpath))
